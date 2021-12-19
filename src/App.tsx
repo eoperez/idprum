ca33a5eec8f8845d3192aa577aa7f23f7aa6e95c
Edwin Perez <Edwin.Perez@blackboard.com>
2021-12-19 13:23:05 -0400

    initial setup for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Grid, Paper, styled, } from '@mui/material';
+import { Menu } from "./Components/Menu";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Menu></Menu>
+        </Grid>
+        <Grid item xs={12}>
+          <Item>xs=12</Item>
+        </Grid>
+      </Grid>
+    </Box>
+
   );
 }
 
